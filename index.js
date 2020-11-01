bb3c8ff6c819637f1f12f520e2ada3bd2241001f
Shelby <shelbyrobison14@gmail.com>
2020-11-01 15:03:20 -0500

    README file generates and title creates upon user input

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+  ]);
+
+const generateReadMe = (answers) => `${answers.title}`;
+
+promptUser()
+  .then((answers) => writeFileAsync("READMETEST.md", generateReadMe(answers)))
+  .then(() => console.log("Successfully wrote to README"))
+  .catch((err) => console.error(err));
